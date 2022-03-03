aee8339427f3ff1872a7a45e29b77ea57bfa6528
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-03-04 01:54:23 +0530

    Create DP_24_Longest_common_substring.cpp

---
+++ DP_24_Longest_common_substring.cpp
@@ -0,0 +1,16 @@
+int longestCommonSubstr (string s1, string s2, int n, int m)
+    {
+        int ct = 0;
+        vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
+        for(int i = 1 ; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= m ; j++)
+            {
+                if(s1[i -1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                ct = max(ct , dp[i][j]);
+            }
+        }
+        return ct;
+        // your code here
+    }
